92f3ef3a407f4ac241fbffa33d3c0e9e18759a80
kongtongxinyiji <1002797232@qq.com>
2023-10-24 14:03:07 +0800

    chengoj-backend-init

---
+++ src/main/java/com/cheng/chengoj/esdao/PostEsDao.java
@@ -0,0 +1,14 @@
+package com.cheng.chengoj.esdao;
+
+import com.cheng.chengoj.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ src/test/java/com/cheng/chengoj/MainApplicationTests.java
@@ -0,0 +1,25 @@
+package com.cheng.chengoj;
+
+import com.cheng.chengoj.config.WxOpenConfig;
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ *
+ * @author <a href="https://github.com/licheng">程序员鱼皮</a>
+ * @from <a href="https://cheng.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Resource
+    private WxOpenConfig wxOpenConfig;
+
+    @Test
+    void contextLoads() {
+        System.out.println(wxOpenConfig);
+    }
+
+}
